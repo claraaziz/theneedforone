ccc25a5608820142bb84752a70c8b7890b698fc5
claraaziz <cocca.az@gmail.com>
2023-11-04 17:45:57 +0200

    edit: new font

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import Main from './components/Main';
 
 function App() {
   return (
-    <div className='h-screen flex items-center justify-center bg-emerald-800'>
+    <div className='h-screen flex items-center justify-center font-Cairo bg-emerald-800'>
       <Main />
     </div>
   );
