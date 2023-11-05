e39a4fabc52caed0c1a617702b246145ab0befe9
claraaziz <cocca.az@gmail.com>
2023-11-05 02:48:26 +0200

    edits: personalized messages

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,7 +3,7 @@ import Second from './Second';
 
 const Main = () => {
 
-    
+    const [num, setNum] = useState(0);
     const [name, setName] = useState("");
     const [click, setClick] = useState(0);
     const [btn, setBtn] = useState(false);
@@ -21,26 +21,43 @@ const Main = () => {
         setName(v);
     }
 
-    console.log(name);
+    const handleBg = (k) => {
+        setNum(k);
+    }
+
+    const links = [
+        "https://www.fullofeyes.com/wp-content/uploads/2017/10/Radiance-768x768.jpg.webp",
+        "https://i.pinimg.com/564x/43/f7/b0/43f7b0917c228ee6013db8d9bfa6d805.jpg",
+        "https://i.pinimg.com/564x/30/97/46/309746dbfd35b8aa61bbcdcdd9f65ec9.jpg",
+        "https://i.pinimg.com/564x/0c/38/92/0c389279671121a3989483a284b7475e.jpg",
+        "https://i.pinimg.com/564x/e9/45/2c/e9452ca0c25a053f28ca16141af7f73c.jpg",
+        "https://i.pinimg.com/564x/25/2a/09/252a0941ef0de4a8842ceff70cff3d01.jpg",
+        "https://i.pinimg.com/564x/76/1b/b5/761bb55619ee819e649b122d3b557c43.jpg",
+        "https://i.pinimg.com/564x/00/ce/65/00ce65cad02f2dc2305a2579be8cb78a.jpg",
+        "https://i.pinimg.com/564x/8e/8e/b3/8e8eb3c7eb2b903a9e461931ba2819bd.jpg",
+        "https://i.pinimg.com/564x/a1/24/a6/a124a639d28b607899bf733605008b19.jpg"
+    ]
+
+    // const link = "https://i.pinimg.com/564x/25/2a/09/252a0941ef0de4a8842ceff70cff3d01.jpg";
 
     switch(click){
         default:
             return <div className="grid justify-items-center duration-500 ease-in-out transform transition-opacity">
-                <p className="text-3xl text-slate-300 mb-8 font-bold lg:text-5xl drop-shadow-lg" dir="rtl">ممكن تكتبي اسمك هنا :)</p>
+                <p className="text-3xl text-slate-300 mb-8 font-bold lg:text-5xl drop-shadow-lg">Please write your name here :)</p>
                 <form className='grid justify-items-center'>
                     <input type="text" className="outline-1 outline-slate-800 shadow-lg rounded-md sm:w-96 sm:h-10 appearance-none py-2 px-3 leading-tight focus:outline-none focus:shadow-outline" onChange={(e) => handleChange(e.target.value)}/>
                     <input type="button" className='bg-emerald-300 shadow-xl text-lg lg:text-2xl pr-7 pl-7 pb-2 pt-2 mt-5 rounded-lg font-bold cursor-pointer' onClick={()=>setClick(1)} value="Go!" disabled={name===""}/>
                 </form>
             </div>
         case 1:
-            return <div className='h-full flex items-center justify-center transition-all duration-500 ease-in-out transform bg-black bg-opacity-60' style={{
-                backgroundImage: 'url("https://i.pinimg.com/564x/25/2a/09/252a0941ef0de4a8842ceff70cff3d01.jpg")',
+            return <div className='h-full w-full flex items-center justify-center transition-all duration-500 ease-in-out transform bg-black bg-opacity-60' style={{
+                backgroundImage: `url(${links[num]})`,
                 backgroundSize: 'cover',
                 backgroundPosition: 'center',
                 backgroundRepeat: 'no',
                 backgroundBlendMode: 'color-burn',
               }}>
-                <div><Second name={name} setClick={clicked} setName={handleName}/></div>
+                <div><Second name={name} setClick={clicked} setName={handleName} setNum={handleBg}/></div>
             </div>
     }
 }
--- src/components/Second.js
+++ src/components/Second.js
@@ -1,15 +1,40 @@
-const Second = ({name, setClick, setName}) => {
+import { useState } from "react";
+
+const Second = ({name, setClick, setName, setNum}) => {
 
     const thename = name;
-    const handleButton = () => {
+
+    const verses = [
+        `For God so loved ${thename} that He gave His only begotten Son, that if ${thename} believes in Him should not perish but have everlasting life. (John 3:16)`,
+        `For God has not given ${thename} a spirit of fear, but of power and of love and of a sound mind. (2 Timothy 1:7)`,
+        `“This is the word of the Lord to ${thename}: ‘Not by might nor by power, but by My Spirit,’ Says the Lord of hosts. (Zechariah 4:6)`,
+        `Behold what manner of love the Father has bestowed on ${thename}, that ${thename} should be called a child of God! (1 John 3:1)`,
+        `Then the word of the Lord came to me, saying: “Before I formed you in the womb I knew you; ${thename}. Before you were born I sanctified you; (Jeremiah 1:5)`,
+        `And Jesus answered and said to her, “${thename}, ${thename}, you are worried and troubled about many things. But one thing is needed (Luke 10:41-42)`,
+        `“Do not say, ‘I am a youth’, ${thename}. For you shall go to all to whom I send you, And whatever I command you, you shall speak. Do not be afraid of their faces, For I am with you to deliver you,” says the Lord. (Jeremiah 1:7-8)`,
+        `“Look, as the clay is in the potter’s hand, so are you in My hand, ${thename}. (Jeremiah 18:6)`,
+        `Have I not commanded you, ${thename}? Be strong and of good courage; do not be afraid, nor be dismayed, for the Lord your God is with you wherever you go. (Joshua 1:9)`,
+        `No temptation has overtaken you except such as is common to man, ${thename}; but God is faithful, who will not allow you to be tempted beyond what you are able, but with the temptation will also make the way of escape, that you may be able to bear it. (1 Corinthians 10:13)`
+    ]
+
+    const getRandomVerse = () => {
+        const i = Math.floor(Math.random() * verses.length);
+        setNum(i);
+        return verses[i];
+      };
+
+      const [verse, setVerse]= useState(getRandomVerse);
+
+      const handleButton = () => {
         setClick(0);
         setName("");
+        const randomVerse = getRandomVerse();
+        setVerse(randomVerse);
     }
 
-    return <div className="grid justify-items-center duration-500 ease-in-out transform transition-opacity" dir="rtl">
+    return <div className="grid justify-items-center duration-500 ease-in-out transform transition-opacity">
         <form className='grid justify-items-center'>
-            <p className="lg:text-5xl drop-shadow-xl text-slate-300 font-bold text-center lg:ml-52 lg:mr-52 leading-normal lg:leading-normal ml-10 mr-10 text-2xl md:text-3xl">"فَأَجَابَ يَسُوعُ وَقَالَ لَها: «{thename}، {thename}، أَنْتِ تَهْتَمِّينَ وَتَضْطَرِبِينَ لأَجْلِ أُمُورٍ كَثِيرَةٍ، وَلكِنَّ الْحَاجَةَ إِلَى وَاحِدٍ.»"</p>
-            <p className="lg:text-5xl drop-shadow-xl text-slate-300  font-bold text-center lg:ml-52 lg:mr-52 leading-normal lg:leading-normal ml-10 mr-10 text-2xl md:text-3xl">(لو 10: 41-42)</p>
+            <p className="lg:text-5xl drop-shadow-xl text-slate-300 font-bold text-center lg:ml-52 lg:mr-52 leading-normal lg:leading-normal ml-10 mr-10 text-2xl md:text-3xl">{verse}</p>
             <input type='button' value="Back" className='bg-emerald-300 shadow-xl lg:text-2xl pr-7 pl-7 pb-2 pt-2 mt-5 rounded-lg font-bold cursor-pointer md:text-xl' onClick={handleButton}/>
         </form>
     </div>
